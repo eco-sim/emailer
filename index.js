32b975f1418c366acc04e8bdc2a30e63de424b4b
eco-sim <125773660+eco-sim@users.noreply.github.com>
2023-11-12 18:26:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 var nodemailer = require('nodemailer');
+const path = require("path");
 
 const app = express();
+app.use(function(req, res, next) {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+		res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+		res.setHeader('Access-Control-Allow-Credentials', true);
+		next();
+});
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -29,13 +37,14 @@ function sendEmail(e, t) {
 
 }
 
-app.get("/", (req, resp) => {resp.send("Hello World!")});
+app.get("/", (req, resp) => {resp.send("Hello World");});
 
 app.post("/email", (request, response) => {
 	var data = request.body;
 	sendEmail('carsonouckama@gmail.com', data.text);
 	response.send({success: true});
 })
+
 
 app.listen(process.env.PORT || 3000, () => console.log('listening at 3000'));
 app.use( express.static('public') );
